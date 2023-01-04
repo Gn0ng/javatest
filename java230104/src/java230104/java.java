f00f4d98d0eecc82072d7f7cd3ae668b62ba10dd
Gn0ng <cbc7697@gmail.com>
2023-01-04 17:37:58 +0900

    열받아

--- java230104/src/java230104/java.java
+++ java230104/src/java230104/java.java
@@ -5,6 +5,8 @@ public class java {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("piss off");
+		//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
+		System.out.println("열받네");
 	}
 
 }
